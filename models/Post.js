f0074fa8067720fba464e99e870b3901ee045dc5
Denva <emsley3684@gmail.com>
2022-01-05 10:54:05 -0500

    added post model, and user associations

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {};
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                len: [5]
+            }
+        },
+        content: {
+            type: DataTypes.STRING,
+            defaultValue: "Click to view article",
+            validate: {
+                len: [3]
+            }
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            validate: {
+                isUrl: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Post;
