5ae1547de3748fc49382c14cca517c3dffabfd64
Denva <emsley3684@gmail.com>
2022-01-08 20:58:28 -0500

    helpers methods added

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const routes = require('./controllers')
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
-const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
