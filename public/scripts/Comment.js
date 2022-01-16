b81c6b1b78e1582fac7f98e2b6c4383215b2266d
Denva <emsley3684@gmail.com>
2022-01-16 14:04:50 -0500

    the programn is ignoring my files lol

--- public/scripts/Comment.js
+++ public/scripts/Comment.js
@@ -1,8 +1,7 @@
-// run functions asynchronously to the server and webpage, to avoid lag time while while javascript funcitons finishes loading and retrieving values
 async function commentForm(event){
     event.preventDefault();
 
-    const comment_text = document.querySelector('textarea[name="comment-text"]').value.trim();
+    const comment_text = document.getElementById("comment-text").value.trim();
     // convert the url address to a string seperate it at the slashes and get the last value for the post id number
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
@@ -26,4 +25,4 @@ async function commentForm(event){
     }
 }
 
-document.querySelector('.comment-form').addEventListener('submit', commentForm)
+document.querySelector(".comment-form").addEventListener('submit', commentForm)
